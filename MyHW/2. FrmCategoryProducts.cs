64db6d5ca42268b176af47d851281c3320fa9686
jason79619@gmail.com <jason79619@gmail.com>
2022-04-12 15:09:42 +0800

    加入專案檔案。

---
+++ MyHW/2. FrmCategoryProducts.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyHomeWork
+{
+    public partial class FrmCategoryProducts : Form
+    {
+        public FrmCategoryProducts()
+        {
+            InitializeComponent();
+            SqlConnection conn = null;
+            try
+            {
+                conn = new SqlConnection("Data Source=.;Initial Catalog=Northwind;Integrated Security=True");
+                conn.Open();
+                SqlCommand command = new SqlCommand("select * from Categories", conn);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    string s = (reader["categoryname"]).ToString() ;
+                   // string s = $"{ reader["categoryname"]}";
+                    comboBox1.Items.Add(s);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=Northwind;Integrated Security=True");
+            conn.Open();
+            SqlCommand command = new SqlCommand("select ");
+        }
+
+        private void FrmCategoryProducts_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
